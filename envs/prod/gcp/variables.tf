617a12932533552356ba15d40799d455b287ef0e
Rushik005 <90761109+Rushik005@users.noreply.github.com>
2026-01-19 13:58:54 +0530

    Create variables.tf

---
+++ envs/prod/gcp/variables.tf
@@ -0,0 +1,19 @@
+variable "project_id" {
+  description = "GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+}
+
+variable "zone" {
+  description = "GCP zone"
+  type        = string
+}
+
+variable "machine_type" {
+  description = "GCE machine type"
+  type        = string
+}
