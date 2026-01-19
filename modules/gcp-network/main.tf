fa0ae79a248e8e0c41aab9d7507db4a64af3f387
Rushik005 <90761109+Rushik005@users.noreply.github.com>
2026-01-19 10:05:50 +0530

    Update main.tf

--- modules/gcp-network/main.tf
+++ modules/gcp-network/main.tf
@@ -1,5 +1,6 @@
 resource "google_compute_network" "vpc" {
   name                    = var.name
+  project                 = var.project_id
   auto_create_subnetworks = false
 }
 
@@ -7,5 +8,6 @@ resource "google_compute_subnetwork" "subnet" {
   name          = "${var.name}-subnet"
   ip_cidr_range = var.subnet_cidr
   region        = var.region
+  project       = var.project_id
   network       = google_compute_network.vpc.id
 }
