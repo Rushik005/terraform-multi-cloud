9209b19e44bd1abf307f884ba62b0f6a34e826f5
Rushik005 <90761109+Rushik005@users.noreply.github.com>
2026-01-19 13:41:04 +0530

    Update main.tf

--- envs/dev/gcp/main.tf
+++ envs/dev/gcp/main.tf
@@ -5,3 +5,17 @@ module "network" {
   region      = var.region
   subnet_cidr = "10.10.0.0/24"
 }
+
+module "vm" {
+  source            = "../../../modules/gcp-gce"
+  name              = "dev-vm"
+  project_id        = var.project_id
+  zone              = "us-central1-a"
+  machine_type      = "e2-medium"
+  subnet_self_link  = module.network.subnet_self_link
+  tags              = ["dev"]
+  labels = {
+    environment = "dev"
+    managed_by  = "terraform"
+  }
+}
